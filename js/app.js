cdf21ffe86bc8f38f8269d7d14eee1f47c47959c
Denii Rincon <JenniferD.Rincon@gmail.com>
2017-05-12 14:23:58 -0400

    committed for lecture purposes

--- js/app.js
+++ js/app.js
@@ -1,55 +1,53 @@
 $(document).ready(function(){
 
-var randomizedOrder = [];
+
 var currentlyFlipped= [];
 var cardsFlipped = 0;
 var cardArr =[];
 
-//functin to shuffle order of cards and store them in new array
 
+//storing divs into the cardArr empty array. using push to place them in card Arr
 for ( let i =0; i < $('.card').length; i++){
   cardArr.push($(".card")[i]);
 }
-
-console.log(cardArr);
-
-function shuffle (cardArr) {
+//functin to shuffle order of cards and store them in new array
+function shuffle (arr) {
   var i = 0
     , j = 0
     , temp = null
 
-  for (i = cardArr.length - 1; i > 0; i -= 1) {
+  for (i = arr.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1))
-    temp = cardArr[i]
-    cardArr[i] = cardArr[j]
-    cardArr[j] = temp
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
   }
-  return cardArr;
-}
-shuffle(cardArr);
-console.log(cardArr);
+  return arr;
+};
 
 
-/////for loop and assigning shuffled cards
-
-
-
-
-// generate game-board
 //on click of a card, should flip
-function newBoard(){
-  currentlyFlipped = 0;
-  shuffle();
-  for (var i =0; i < cards.length; i++){
-    $(".card").on("click",function(){
 
-    })
-  }
+function clickedCard(){
+  cardsFlipped = 0;
+  shuffle(cardArr);
+  $('.card').on("click",function(){
+    firstPick = $(this).attr('id');
+    $(this).show();
+    currentlyFlipped.push();
+    cardsFlipped++;
+  });
 }
+
+///cf 0 to cf 1//
+clickedCard();
+
+/*
+clickedCard();
+console.log(clickedCard());
+*/
+/*
 //Look for Pair id, if a match remove card from board and add point to player_score
-var clicks = 0;
-var firstPick="";
-var secondPick= "";
 
 if(firstPick === secondPick){
   console.log("Found a match");
@@ -61,6 +59,7 @@ if(firstPick === secondPick){
 //toggle player
 //click two and after a fews it will face down again
 
+*/
 
 
 
