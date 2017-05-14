2fffb4bfc3f9e09b3f21c2f5d50aea9d307338e4
Denii Rincon <JenniferD.Rincon@gmail.com>
2017-05-14 15:58:15 -0400

    working on matching function

--- js/app.js
+++ js/app.js
@@ -4,19 +4,20 @@ $(document).ready(function(){
 var currentlyFlipped= [];
 var cardsFlipped = 0;
 var cardArr =[];
-
+var score = 0;
 
 //storing divs into the cardArr empty array. using push to place them in card Arr
-for ( let i =0; i < $('.card').length; i++){
-  cardArr.push($(".card")[i]);
+for ( let i =0; i < $('.card-container').length; i++){
+  cardArr.push($('.card-container')[i]);
 }
+
 //functin to shuffle order of cards and store them in new array
 function shuffle (arr) {
   var i = 0
     , j = 0
     , temp = null
 
-  for (i = arr.length - 1; i > 0; i -= 1) {
+  for (i = cardArr.length - 1; i > 0; i -= 1) {
     j = Math.floor(Math.random() * (i + 1))
     temp = arr[i]
     arr[i] = arr[j]
@@ -25,28 +26,42 @@ function shuffle (arr) {
   return arr;
 };
 
+shuffle(cardArr);
+console.log(shuffle(cardArr));
 
 //on click of a card, should flip
-
 function clickedCard(){
   cardsFlipped = 0;
   shuffle(cardArr);
-  $('.card').on("click",function(){
+  $('.card-container').on("click",function(){
+    $(this).children().eq(0).show();
+    console.log($(this).children());
     firstPick = $(this).attr('id');
-    $(this).show();
     currentlyFlipped.push();
     cardsFlipped++;
+    if(cardsFlipped === 2){
+      $('.card-container').off("click");
+
+      checkMatch();
+    }
+  //  console.log(cardsFlipped);
   });
+
+} //end of clicked Card function
+clickedCard();
+
+var checkMatch = function(){
+  if (currentlyFlipped[0] !== currentlyFlipped[1]){
+    console.log("try again");
+  } else {
+    console.log("found a match");
+  }
 }
 
-///cf 0 to cf 1//
-clickedCard();
 
 /*
-clickedCard();
-console.log(clickedCard());
-*/
-/*
+
+
 //Look for Pair id, if a match remove card from board and add point to player_score
 
 if(firstPick === secondPick){
